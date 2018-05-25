8d2923a79c0b328d4d6b9b2f12ed9f2b9d3b6584
Nguyen Huu Kim <nguyen.huu.kim@framgia.com>
2018-05-25 16:43:57 +0700

    Fix Gist embed

--- src/markdown/plugins/embeds/gist.ts
+++ src/markdown/plugins/embeds/gist.ts
@@ -1,24 +1,11 @@
-const parseGistURL = (code: string): string => {
-    if (code.startsWith('https://')) {
-        const regExp = /^(?:.*gist\.github\.com\/)([a-zA-Z0-9-]{0,38})\/([a-f0-9]*)$/;
-        const match = code.match(regExp);
-
-        if (!match) {
-            return '';
-        }
-
-        return `https://gist.github.com/${match[0]}.js`;
-    }
-
-    return code;
-};
-
 export default (code: string): string => {
-    const embedURL = parseGistURL(code);
-    if (!embedURL) {
+    const regExp = /^(?:.*gist\.github\.com\/)([a-zA-Z0-9-]{0,38}\/[a-f0-9]+)$/;
+    const match = code.match(regExp);
+    if (!match) {
         return '';
     }
 
+    const embedURL = `https://gist.github.com/${match[1]}.js`;
     const id = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10);
     const gistFrameHTML = `<html><body><script src='${embedURL}'></script></body></html>`;
 
