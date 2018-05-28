0762aae2e43d3de7b6caf25867002e32c0718685
Nguyen Huu Kim <nguyen.huu.kim@framgia.com>
2018-05-28 09:07:32 +0700

    Implement SlideShare embed by URL

--- src/markdown/plugins/embeds/slideshare.ts
+++ src/markdown/plugins/embeds/slideshare.ts
@@ -1,13 +1,27 @@
-const parseSlideshareURL = (code: string): string => {
-    if (!code.startsWith('http') && !code.match(/^(http|https):\/\/slideshare.net\/slideshow\/embed_code\/.*/)) {
-        return `https://slideshare.net/slideshow/embed_code/${encodeURIComponent(code)}`;
+const parseSlideShareURL = (code: string, baseURL: string): string => {
+    const isSlideShareID = /^([0-9])+$/.test(code);
+    const isSlideShareURL = /^(?:(?:http|https):\/\/)?(?:www\.)?(slideshare\.net\/.+)$/.test(code);
+    const matchesEmbed = code.match(/^(?:(?:http|https):\/\/)?(?:www\.)?(slideshare\.net\/slideshow\/embed_code\/.+)$/);
+
+    if (isSlideShareID) {
+        return `https://slideshare.net/slideshow/embed_code/${code}`;
     }
 
-    return code;
+    if (!isSlideShareURL) {
+        return '';
+    }
+
+    if (matchesEmbed) {
+        return `https://${matchesEmbed[1]}`;
+    }
+
+    return `${baseURL}/embed/slideshare/?url=${code}`;
 };
 
-export default (code: string): string => {
-    const embedURL = parseSlideshareURL(code);
+export default ({ baseURL }) => (code: string): string => {
+    const embedURL = parseSlideShareURL(code, baseURL);
+
+    if (!embedURL) return code;
 
     return `<div class="embed-responsive embed-responsive-16by9">
         <iframe
