6cf6e3891451ec2f331e549f61b324508f42167f
Truong Hoang Phuong <thp.1093@outlook.com>
2018-06-07 16:30:56 +0700

    Update

--- markdown/plugins/highlight.js
+++ markdown/plugins/highlight.js
@@ -1,77 +1,70 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Prism = require("prismjs");
-var loadLanguages = require("prismjs/components/index");
 var utils_1 = require("markdown-it/lib/common/utils");
-loadLanguages([
-    'actionscript',
-    'apacheconf',
-    'cpp',
-    'arduino',
-    'asciidoc',
-    'bash',
-    'basic',
-    'clojure',
-    'coffeescript',
-    'csharp',
-    'd',
-    'dart',
-    'diff',
-    // 'django',
-    'docker',
-    'elixir',
-    'elm',
-    'erb',
-    'erlang',
-    'gherkin',
-    'go',
-    'graphql',
-    'haml',
-    'handlebars',
-    'haskell',
-    'haxe',
-    'hsts',
-    'http',
-    'ini',
-    'java',
-    'json',
-    'jsx',
-    'kotlin',
-    'latex',
-    'less',
-    'lisp',
-    'livescript',
-    'lua',
-    'makefile',
-    'markdown',
-    'matlab',
-    'nginx',
-    'nix',
-    'objectivec',
-    'pascal',
-    'perl',
-    'php',
-    'plsql',
-    'powershell',
-    'protobuf',
-    'python',
-    'q',
-    'r',
-    'ruby',
-    'rust',
-    'sass',
-    'scala',
-    'scss',
-    'sql',
-    'stylus',
-    'swift',
-    'twig',
-    'typescript',
-    'vbnet',
-    'vim',
-    'wasm',
-    'yaml',
-]);
+require('prismjs/components/prism-actionscript');
+require('prismjs/components/prism-c');
+require('prismjs/components/prism-cpp');
+require('prismjs/components/prism-arduino');
+require('prismjs/components/prism-bash');
+require('prismjs/components/prism-basic');
+require('prismjs/components/prism-clojure');
+require('prismjs/components/prism-coffeescript');
+require('prismjs/components/prism-csharp');
+require('prismjs/components/prism-d');
+require('prismjs/components/prism-dart');
+require('prismjs/components/prism-diff');
+// require('prismjs/components/prism-django');
+require('prismjs/components/prism-docker');
+require('prismjs/components/prism-elixir');
+require('prismjs/components/prism-elm');
+require('prismjs/components/prism-markup-templating');
+require('prismjs/components/prism-ruby');
+require('prismjs/components/prism-erb');
+require('prismjs/components/prism-erlang');
+require('prismjs/components/prism-gherkin');
+require('prismjs/components/prism-go');
+require('prismjs/components/prism-graphql');
+require('prismjs/components/prism-haml');
+require('prismjs/components/prism-handlebars');
+require('prismjs/components/prism-haskell');
+require('prismjs/components/prism-haxe');
+require('prismjs/components/prism-http');
+require('prismjs/components/prism-ini');
+require('prismjs/components/prism-java');
+require('prismjs/components/prism-json');
+require('prismjs/components/prism-kotlin');
+require('prismjs/components/prism-latex');
+require('prismjs/components/prism-less');
+require('prismjs/components/prism-lisp');
+require('prismjs/components/prism-livescript');
+require('prismjs/components/prism-lua');
+require('prismjs/components/prism-makefile');
+require('prismjs/components/prism-matlab');
+require('prismjs/components/prism-nginx');
+require('prismjs/components/prism-nix');
+require('prismjs/components/prism-objectivec');
+require('prismjs/components/prism-pascal');
+require('prismjs/components/prism-perl');
+require('prismjs/components/prism-php');
+require('prismjs/components/prism-sql');
+require('prismjs/components/prism-plsql');
+require('prismjs/components/prism-powershell');
+require('prismjs/components/prism-protobuf');
+require('prismjs/components/prism-python');
+require('prismjs/components/prism-q');
+require('prismjs/components/prism-r');
+require('prismjs/components/prism-rust');
+require('prismjs/components/prism-sass');
+require('prismjs/components/prism-scala');
+require('prismjs/components/prism-scss');
+require('prismjs/components/prism-stylus');
+require('prismjs/components/prism-swift');
+require('prismjs/components/prism-twig');
+require('prismjs/components/prism-typescript');
+require('prismjs/components/prism-vbnet');
+require('prismjs/components/prism-wasm');
+require('prismjs/components/prism-yaml');
 function createHighlighter(options) {
     return function (str, lang) {
         var prismLang = Prism.languages[lang];
